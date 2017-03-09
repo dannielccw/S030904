89714db388d30857fbba2ca70c4d5b361e3900bc
dannielccw <dannielccw@gmail.com>
2017-03-09 13:18:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/s030904/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.yvtc.s030904;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class MainActivity extends AppCompatActivity {
+    AutoCompleteTextView act;
+    String str[] = {"AAA","AAB","ABC","ABB","CCC","BBC","BBB"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        act = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, str);
+        act.setAdapter(adapter);
+        act.setThreshold(1);
+        
+    }
+}
