98d9dc5aced42f8625fd0f7981ffe59f0f860e76
dannielccw <dannielccw@gmail.com>
2017-03-09 14:01:26 +0800

    change ImageView picture

--- app/src/main/java/com/example/yvtc/s030904/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s030904/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.yvtc.s030904;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     AutoCompleteTextView act;
     String str[] = {"AAA","AAB","ABC","ABB","CCC","BBC","BBB"};
+
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +20,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         act = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        iv = (ImageView) findViewById(R.id.imageView);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, str);
         act.setAdapter(adapter);
         act.setThreshold(1);
-        
     }
+
+    public void click1(View v)
+    {
+        iv.setImageResource(R.drawable.flower1);
+
+    }
+    public void click2(View v)
+    {
+        iv.setImageResource(R.drawable.flower2);
+    }
+
 }
